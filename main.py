92a1c5f25c9f1acd6ea801167ec40da6e5008195
AbisheakJacob <abisheakjacob0032@gmail.com>
2023-11-06 13:10:14 +0530

    a simple model of flask app was made
    Assessed #4

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-from constraints.constraint_1 import add
+# importing the packagaes
+from flask import Flask
 
+# create an instance of the flask application
+app = Flask(__name__)
 
-print('Output : ', add(2,3))
+# homepage - app route
+@app.route('/')
+def index():
+    return 'Welcome to Crew Scheduling App!'
+
+# second page - # assume module one page
+@app.route('/optimize')
+def optimize_crew_schedule():
+    # Call your optimization function here
+    # Example:
+    # result = solve_routing_problem()
+    return 'Optimization completed'  # Return the result
+
+# initialize the application
+if __name__ == '__main__':
+    app.run(debug=True)
