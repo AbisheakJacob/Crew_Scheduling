c0f9418ccb7b403b70daa0cbfc190afe60bd9a88
AbisheakJacob <abisheakjacob0032@gmail.com>
2023-11-21 10:49:07 +0530

    duties were generated

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # importing the packages
 from data.data_creation import create_data
-from pairing_generation.pairing_generation import generate_duties
+from pairing_generation.duty_generation import generate_duties
+
 
 def main():
-
     # create the data for generating the pairs
     # inputs are (number_of_days, num_of_flight_legs_per_day)
     create_data()
