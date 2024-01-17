f205eede0514790fbab8a14683ecd8c462db9b49
Abisheak Jacob J <abisheakjacob0032@gmail.com>
2024-01-17 14:40:26 +0530

    changes made to pairing generation

--- crew_pairing/column_gen.py
+++ crew_pairing/column_gen.py
@@ -24,7 +24,7 @@ def RMP(index, num_flights, pair_matrix, cost_matrix):
     # declaring the constraints
     # Uniqueness of flight legs and all flight legs covered
     for i in range(num_flights):
-        solver.Add(solver.Sum(result_matrix[:, i]) >= 1)
+        solver.Add(solver.Sum(result_matrix[:, i]) == 1)
 
     # declaring the objective function
     solver.Minimize(np.sum(cost_matrix * x))
@@ -36,7 +36,9 @@ def RMP(index, num_flights, pair_matrix, cost_matrix):
     x_values = [x[i][0].solution_value() for i in range(len(index))]
 
     # find the indices of the final selected pairs
-    optimal_index = [value for value, binary_value in zip(index, x_values) if binary_value == 1]
+    optimal_index = [
+        value for value, binary_value in zip(index, x_values) if binary_value == 1
+    ]
 
     return status, solver.Objective().Value(), optimal_index
 
@@ -50,7 +52,9 @@ def sub_problem(pair_matrix, cost_matrix, index, num_pairs, num_flights):
     solver = pywraplp.Solver.CreateSolver("GLOP")
 
     # creating the binary allocation variable
-    x = np.array([solver.NumVar(0, 1, f"x_{i}") for i in range(len(index))]).reshape(-1, 1)
+    x = np.array([solver.NumVar(0, 1, f"x_{i}") for i in range(len(index))]).reshape(
+        -1, 1
+    )
 
     # create a matrix that is the product of the decision variable and the pair matrix
     result_matrix = x * pair_matrix_idx
@@ -58,7 +62,7 @@ def sub_problem(pair_matrix, cost_matrix, index, num_pairs, num_flights):
     # declaring the constraints
     # Uniqueness of flight legs and all flight legs covered
     for i in range(num_flights):
-        solver.Add(solver.Sum(result_matrix[:, i]) >= 1)
+        solver.Add(solver.Sum(result_matrix[:, i]) == 1)
 
     # declaring the objective function
     solver.Minimize(np.sum(cost_matrix_idx * x))
@@ -67,13 +71,17 @@ def sub_problem(pair_matrix, cost_matrix, index, num_pairs, num_flights):
     status = solver.Solve()
 
     # find the values of the decision variable
-    dual_values = np.array([constraint.dual_value() for constraint in solver.constraints()])
+    dual_values = np.array(
+        [constraint.dual_value() for constraint in solver.constraints()]
+    )
 
     # find the reduced cost matrix
     red_cost_mtx = []
 
     for i in range(num_pairs):
-        red_cost_mtx.append(cost_matrix[i] - np.sum(pair_matrix[i] * dual_values, axis=0))
+        red_cost_mtx.append(
+            cost_matrix[i] - np.sum(pair_matrix[i] * dual_values, axis=0)
+        )
 
     return red_cost_mtx, np.argsort(red_cost_mtx)[:50].tolist()
 
@@ -82,7 +90,9 @@ def sub_problem(pair_matrix, cost_matrix, index, num_pairs, num_flights):
 def column_gen():
     # reading the cost_matrix and the pair_matrix
     cost_matrix = np.loadtxt("data/cost_matrix/cost.txt")
-    pair_matrix = np.genfromtxt("data/pairings/pair_array.txt", delimiter=',', dtype='int')
+    pair_matrix = np.genfromtxt(
+        "data/pairings/pair_array.txt", delimiter=",", dtype="int"
+    )
 
     # determining the number of flights and tasks
     num_pairs = pair_matrix.shape[0]
@@ -97,7 +107,9 @@ def column_gen():
         # create a reduced pair array that contains atleast one feasible solution
         index = np.argsort(cost_matrix)[:ini_pair].tolist()
 
-        if np.all(np.sum(pair_matrix[index], axis=0) >= 1) == True:
+        status, obj, idx = RMP(index, num_flights, pair_matrix, cost_matrix)
+
+        if obj != 0.0:
             break
 
         ini_pair += increment
@@ -110,12 +122,13 @@ def column_gen():
 
     # the column generation problem
     while True:
-
         # the RMP solves the problem for a small number of pairs
         status, obj, idx = RMP(index, num_flights, pair_matrix, cost_matrix)
 
         # the sub problem finds the reduced cost matrix and the index of the least reduced cost
-        red_cost_mtx, least_red_cost_index = sub_problem(pair_matrix, cost_matrix, index, num_pairs, num_flights)
+        red_cost_mtx, least_red_cost_index = sub_problem(
+            pair_matrix, cost_matrix, index, num_pairs, num_flights
+        )
 
         index = index + least_red_cost_index
 
