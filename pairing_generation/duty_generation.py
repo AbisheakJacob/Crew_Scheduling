465ec5c184621d5dfe2c78fffe3aa9bee21077f5
Abisheak Jacob J <abisheakjacob0032@gmail.com>
2024-02-12 19:59:40 +0530

    duty gen jupyter file removed

--- pairing_generation/duty_generation.py
+++ pairing_generation/duty_generation.py
@@ -1,3 +1,4 @@
+# %%
 # importing the packages
 import numpy as np
 import pandas as pd
@@ -6,57 +7,86 @@ from warnings import filterwarnings
 filterwarnings("ignore")
 
 
-def generate_duties_dfs(np_arr, current_duty, valid_duty):
-    if is_valid_duty(np_arr, current_duty):
+# %%
+#  DFS function to generate all possible duties
+def generate_duties_dfs(np_arr, current_duty, valid_duty, depth):
+    if is_valid_duty(np_arr, current_duty, depth):
         valid_duty.append(current_duty.copy().tolist())
         return
-    if len(current_duty) == 3:
+    if len(current_duty) == depth:
         return
 
-    # Recursive DFS for each remaining flight leg
     for index in np.where(np_arr[:, 1] == np_arr[current_duty[-1]][2])[0]:
-        # Check if the leg can be added to the current pair
         if sub_df(np_arr, current_duty, index):
             current_duty = np.append(current_duty, index)
-            generate_duties_dfs(np_arr, current_duty, valid_duty)
+            generate_duties_dfs(np_arr, current_duty, valid_duty, depth)
             current_duty = current_duty[:-1]  # Backtrack
 
 
+# %%
 # function taking care of the time constraint and total time constraint
 def sub_df(np_arr, current_duty, index):
-    time_difference = np_arr[index][3] - np_arr[current_duty[-1]][4]
-    return ((time_difference >= np.timedelta64(1, "h"))) and (
-        (time_difference <= np.timedelta64(3, "h"))
+    return (
+        np.timedelta64(1, "h")
+        <= (np_arr[index][3] - np_arr[current_duty[-1]][4])
+        <= np.timedelta64(4, "h")
     )
 
 
 # total time constraint and return to homebase constraint
-def is_valid_duty(np_arr, current_duty):
-    # Implement conditions for pair validity
+def is_valid_duty(np_arr, current_duty, depth):
     return (
-        np_arr[current_duty[0]][1] == np_arr[current_duty[-1]][2]
-    ) and (  # return to homebase condition
-        (np_arr[current_duty[-1]][4] - np_arr[current_duty[0]][3])
-        <= np.timedelta64(8, "h")
+        len(current_duty) == depth
+        and (np_arr[current_duty[0]][1] == np_arr[current_duty[-1]][2])
+        and (
+            (np_arr[current_duty[-1]][4] - np_arr[current_duty[0]][3])
+            <= np.timedelta64(10, "h")
+        )
     )
 
 
-def generate_duties():
-    # reading the dataframe
-    df = pd.read_csv("data/flight_legs/data.csv")
-    df["start_time"] = pd.to_datetime(df["start_time"], format="%Y-%m-%d %H:%M:%S")
-    df["end_time"] = pd.to_datetime(df["end_time"], format="%Y-%m-%d %H:%M:%S")
-
-    # convert the dataframe to a numpy array
-    np_arr = df.to_numpy()
-
+# %%
+# function to generate all possible duties
+def generate_duties(depth=3):
+    # Reading the dataframe and converting it to a numpy array
+    np_arr = pd.read_csv(
+        "../data/flight_legs/data.csv",
+        parse_dates=["start_time", "end_time"],
+        infer_datetime_format=True,
+    ).to_numpy()
     # Create an empty list to hold the duties
     valid_duty_dfs = []
 
     # Start DFS from each flight leg
     for start_leg in range(len(np_arr)):
-        generate_duties_dfs(np_arr, np.array([start_leg]), valid_duty_dfs)
+        generate_duties_dfs(np_arr, np.array([start_leg]), valid_duty_dfs, depth)
 
     # Open the file in write mode
-    with open(f"data/duties/duties.txt", "w") as file:
+    with open(f"../data/duties/duties.txt", "w") as file:
         file.writelines(f"{item}\n" for item in valid_duty_dfs)
+
+
+# %%
+# Driver code
+generate_duties(depth=3)
+
+
+# %%
+# define the function to check if all the flight legs are covered
+def duty_check():
+    # Open the file in read mode
+    with open("../data/duties/duties.txt", "r") as file:
+        lines = file.readlines()
+
+    # convert each line (representing a list) to an actual list
+    duties = [eval(line) for line in lines]
+
+    from itertools import chain
+
+    duty = list(set(chain(*duties)))
+
+    print(len(duties), len(duty))
+
+
+# call the duty check function
+duty_check()
